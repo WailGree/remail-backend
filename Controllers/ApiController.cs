137cab449b24342775276e12f60354ee10806921
Mate Szathmari <mateszathmari@gmail.com>
2021-02-24 07:20:20 +0100

    user cretential

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -14,5 +14,16 @@ namespace Remail_backend.Controllers
         {
             return Ok();
         }
+
+        // POST: api/Login
+        //[HttpPost("Login")]
+        //public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        //{
+        //    _context.TodoItems.Add(todoItem);
+        //    await _context.SaveChangesAsync();
+
+        //    //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        //    return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        //}
     }
 }

e01cf79cc5d530337dc225228c90879fdab339e6
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-02-24 20:53:53 +0100

    remove unused directives in solution

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Remail_backend.Models;
 using RemailCore.Models;
--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Remail_backend
 {
@@ -18,9 +12,6 @@ namespace Remail_backend
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }

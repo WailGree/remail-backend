cfa1bcba8bfc930c12ec4064a9b105b15c36d151
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-03-25 09:29:23 +0100

    Move files, create Tests project

---
+++ Remail-backend.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NUnit.Framework;
+
+//namespace Remail_backend.Tests
+//{
+//    public class Tests
+//    {
+//        [SetUp]
+//        public void Setup()
+//        {
+//        }
+
+//        [Test]
+//        public void Test1()
+//        {
+//            Assert.Pass();
+//        }
+//    }
+//}

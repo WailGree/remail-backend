2c7482af56a69ca0d6f67e32b1be7af5352c997b
Roland Dominik Szilagyi <szilagyi.roland.dominik@gmail.com>
2021-03-25 11:45:41 +0100

    Test for getting emails

--- Remail-backend.Tests/ApiControllerTests.cs
+++ Remail-backend.Tests/ApiControllerTests.cs
@@ -1,12 +1,38 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Xunit;
 
 namespace Remail_backend.Tests
 {
-    class ApiControllerTests
+    class ApiControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
+        private HttpClient _client { get; }
+
+        public ApiControllerTests()
+        {
+            var fixture = new WebApplicationFactory<Startup>();
+            _client = fixture.CreateClient();
+        }
+
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public async Task Post_Should_Get_Emails()
+        {
+            var stringContent = new StringContent(JsonConvert.SerializeObject(""), Encoding.UTF8,
+                "application/x-www-form-urlencoded");
+            var response = await _client.PostAsync("api/get-mails", stringContent);
+            Assert.IsTrue(response.StatusCode == HttpStatusCode.OK);
         }
     }
 }
